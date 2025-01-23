41d2b3f9a10cc00ade22d6e1d9aa767d53cd4c35
Mohit Patoliya <mohitpatoliyadev3820@gmail.com>
2025-01-23 14:15:46 +0530

    table setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
 import Image from "next/image";
 
 export default function Home() {
+  const dummyData = [
+    { id: 1, name: 'Alice', email: 'alice@example.com', age: 25 },
+    { id: 2, name: 'Bob', email: 'bob@example.com', age: 30 },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com', age: 35 },
+    { id: 4, name: 'Diana', email: 'diana@example.com', age: 28 },
+  ];
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   
+   <div className="overflow-x-auto">
+      <table className="table-auto border-collapse border border-gray-300 w-full">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border border-gray-300 px-4 py-2">ID</th>
+            <th className="border border-gray-300 px-4 py-2">Name</th>
+            <th className="border border-gray-300 px-4 py-2">Email</th>
+            <th className="border border-gray-300 px-4 py-2">Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyData.map((row) => (
+            <tr className="text-center">
+              <td className="border border-gray-300 px-4 py-2">{row.id}</td>
+              <td className="border border-gray-300 px-4 py-2">{row.name}</td>
+              <td className="border border-gray-300 px-4 py-2">{row.email}</td>
+              <td className="border border-gray-300 px-4 py-2">{row.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </div>
   );
 }
