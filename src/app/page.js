35853a0d87f706721921d43fed9a651ef6428b2d
Mohit Patoliya <mohitpatoliyadev3820@gmail.com>
2025-01-23 14:18:43 +0530

    husky install

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   const dummyData = [
-    { id: 1, name: 'Alice', email: 'alice@example.com', age: 25 },
+    { id: 1, name: 'Alice', age: 25 },
     { id: 2, name: 'Bob', email: 'bob@example.com', age: 30 },
     { id: 3, name: 'Charlie', email: 'charlie@example.com', age: 35 },
     { id: 4, name: 'Diana', email: 'diana@example.com', age: 28 },
