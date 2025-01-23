44a89af28c76d9e4d8758062c1cc37ab22b066f1
Mohit Patoliya <mohitpatoliyadev3820@gmail.com>
2025-01-23 15:13:37 +0530

    feat: initial commit with husky setup

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   const dummyData = [
-    { id: 1, name: 'Alice', age: 25 },
+    { id: 1, name: 'Alice', email: 'alice@example.com', age: 25 },
     { id: 2, name: 'Bob', email: 'bob@example.com', age: 30 },
     { id: 3, name: 'Charlie', email: 'charlie@example.com', age: 35 },
     { id: 4, name: 'Diana', email: 'diana@example.com', age: 28 },
   ];
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   <div className="overflow-x-auto">
+    <div className="overflow-x-auto">
       <table className="table-auto border-collapse border border-gray-300 w-full">
         <thead>
           <tr className="bg-gray-200">
@@ -21,7 +21,7 @@ export default function Home() {
         </thead>
         <tbody>
           {dummyData.map((row) => (
-            <tr className="text-center">
+            <tr key={row.id} className="text-center">
               <td className="border border-gray-300 px-4 py-2">{row.id}</td>
               <td className="border border-gray-300 px-4 py-2">{row.name}</td>
               <td className="border border-gray-300 px-4 py-2">{row.email}</td>
